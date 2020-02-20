cbef3697e4de7f9b9efc4873fa88b5ee60365387
Samil <Samil@DESKTOP-PG28H37>
2020-02-20 11:37:21 +0300

    Add project files.

---
+++ MVCApiBased/Controllers/PersonelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApiBased.Controllers
+{
+    public class PersonelController : Controller
+    {
+        string[] Isimler = { "Ahmet", "Mehmet", "Süreyya" };
+        public ActionResult Index()
+        {
+
+            return Json(Isimler);
+        }
+    }
+}
